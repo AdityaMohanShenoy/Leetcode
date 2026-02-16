22f3725e54c1f46a9e7d2ed599abafdf6337569f
Aditya Mohan Shenoy <pes1ug23cs033@pesu.pes.edu>
2026-02-16 09:29:20 +0530

    Sync solution: Max Area of Island

---
+++ medium/max-area-of-island/solution.py
@@ -0,0 +1,63 @@
+"""
+[Description]
+Max Area of Island
+https://leetcode.com/problems/max-area-of-island/
+
+You are given an m x n binary matrix grid. An island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.) You may assume all four edges of the grid are surrounded by water.
+
+The area of an island is the number of cells with a value 1 in the island.
+
+Return the maximum area of an island in grid. If there is no island, return 0.
+
+ 
+Example 1:
+
+Input: grid = [[0,0,1,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,1,1,0,1,0,0,0,0,0,0,0,0],[0,1,0,0,1,1,0,0,1,0,1,0,0],[0,1,0,0,1,1,0,0,1,1,1,0,0],[0,0,0,0,0,0,0,0,0,0,1,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,0,1,1,0,0,0,0]]
+Output: 6
+Explanation: The answer is not 11, because the island must be connected 4-directionally.
+
+Example 2:
+
+Input: grid = [[0,0,0,0,0,0,0,0]]
+Output: 0
+
+ 
+Constraints:
+
+  m == grid.length
+  n == grid[i].length
+  1 <= m, n <= 50
+  grid[i][j] is either 0 or 1.
+
+[Metadata]
+- Difficulty: Medium
+- Topics: Array, Depth-First Search, Breadth-First Search, Union-Find, Matrix
+- Slug: max-area-of-island
+"""
+
+// [Solution]
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        def dfs(r,c):
+            if r < 0 or r >= rows or c<0 or c>= columns or grid[r][c] == 0 :
+                return 0
+            grid[r][c] = 0 
+            return (
+                1
+                + dfs(r - 1, c)
+                + dfs(r + 1, c)
+                + dfs(r, c - 1)
+                + dfs(r, c + 1)
+            )
+
+        rows = len(grid)
+        columns = len(grid[0])
+        maxarea = 0     
+
+        for r in range(rows):
+            for c in range(columns):
+                if grid[r][c]!= 0 :
+                    area = dfs(r, c)
+                    maxarea = max(area, maxarea)
+
+        return maxarea
