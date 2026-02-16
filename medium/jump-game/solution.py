2c36049ef178d2457b724d78e38462c970aea822
Aditya Mohan Shenoy <pes1ug23cs033@pesu.pes.edu>
2026-02-16 09:26:14 +0530

    Sync solution: Jump Game

---
+++ medium/jump-game/solution.py
@@ -0,0 +1,56 @@
+"""
+[Description]
+Jump Game
+https://leetcode.com/problems/jump-game/
+
+You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+
+Return true if you can reach the last index, or false otherwise.
+
+ 
+Example 1:
+
+Input: nums = [2,3,1,1,4]
+Output: true
+Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+
+Example 2:
+
+Input: nums = [3,2,1,0,4]
+Output: false
+Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
+
+ 
+Constraints:
+
+  1 <= nums.length <= 104
+  0 <= nums[i] <= 105
+
+[Metadata]
+- Difficulty: Medium
+- Topics: Array, Dynamic Programming, Greedy
+- Slug: jump-game
+"""
+
+// [Solution]
+class Solution(object):
+    def canJump(self, nums):
+
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        goal = 1 
+        for i in range(len(nums)-2 , -1 ,-1 ):
+            if nums[i] >= goal :
+                goal = 1
+            else:
+                goal += 1 
+    
+        
+        return goal == 1
+
+
+
+
+
