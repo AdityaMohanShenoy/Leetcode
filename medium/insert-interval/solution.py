12b7577aeb213c9a6308c8599339f140baea85ab
Aditya Mohan Shenoy <pes1ug23cs033@pesu.pes.edu>
2026-03-09 21:03:26 +0530

    Sync solution: Insert Interval

---
+++ medium/insert-interval/solution.py
@@ -0,0 +1,67 @@
+"""
+[Description]
+Insert Interval
+https://leetcode.com/problems/insert-interval/
+
+You are given an array of non-overlapping intervals intervals where intervals[i] = [starti, endi] represent the start and the end of the ith interval and intervals is sorted in ascending order by starti. You are also given an interval newInterval = [start, end] that represents the start and end of another interval.
+
+Insert newInterval into intervals such that intervals is still sorted in ascending order by starti and intervals still does not have any overlapping intervals (merge overlapping intervals if necessary).
+
+Return intervals after the insertion.
+
+Note that you don't need to modify intervals in-place. You can make a new array and return it.
+
+ 
+Example 1:
+
+Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+Output: [[1,5],[6,9]]
+
+Example 2:
+
+Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+Output: [[1,2],[3,10],[12,16]]
+Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
+
+ 
+Constraints:
+
+  0 <= intervals.length <= 104
+  intervals[i].length == 2
+  0 <= starti <= endi <= 105
+  intervals is sorted by starti in ascending order.
+  newInterval.length == 2
+  0 <= start <= end <= 105
+
+[Metadata]
+- Difficulty: Medium
+- Topics: Array
+- Slug: insert-interval
+"""
+
+// [Solution]
+class Solution(object):
+    def insert(self, intervals, newInterval):
+        res = []
+        i = 0
+        n = len(intervals)
+
+        # 1) add all intervals that end before newInterval starts
+        while i < n and intervals[i][1] < newInterval[0]:
+            res.append(intervals[i])
+            i += 1
+
+        # 2) merge all intervals that overlap with newInterval
+        start, end = newInterval[0], newInterval[1]
+        while i < n and intervals[i][0] <= end:
+            start = min(start, intervals[i][0])
+            end = max(end, intervals[i][1])
+            i += 1
+        res.append([start, end])
+
+        # 3) add the rest
+        while i < n:
+            res.append(intervals[i])
+            i += 1
+
+        return res
